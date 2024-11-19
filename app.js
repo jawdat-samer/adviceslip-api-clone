d9d88140995ee8fa824e5e57a073b7008c9d90cc
jawdat-samer <112759993+jawdat-samer@users.noreply.github.com>
2024-11-19 02:39:35 +0200

    init

---
+++ app.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const { rateLimit } = require('express-rate-limit');
+const hpp = require('hpp');
+const xss = require('xss-clean');
+const cors = require('cors');
+const compression = require('compression');
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
+
+const app = express();
+
+app.use(cors());
+app.options('*', cors());
+
+app.use(
+  helmet({
+    contentSecurityPolicy: false,
+  })
+);
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+const limitter = rateLimit({
+  limit: 100,
+  windowMs: 15 * 60 * 1000,
+  message: 'Too many requests from this IP!',
+  legacyHeaders: false,
+});
+
+app.use(limitter);
+
+app.use(express.json({ limit: '10kb' }));
+app.use(express.urlencoded({ limit: '10kb', extended: true }));
+
+app.use(hpp());
+
+app.use(xss());
+
+app.use(compression());
+
+app.all('*', (req, res, next) => {
+  return next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
+app.use(globalErrorHandler);
+
+module.exports = app;
---
+++ controllers/errorController.js
@@ -0,0 +1,35 @@
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+    error: err,
+    stack: err.stack,
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  if (err.isOperational === true) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  } else {
+    console.log(err);
+
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went wrong!',
+    });
+  }
+};
+
+module.exports = (err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'fail';
+
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    sendErrorProd(err, res);
+  }
+};
